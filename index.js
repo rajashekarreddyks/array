92a0a062b2cd1337e41e8c23b7f95c918b8ecf9b
shekar <rajksreddy@gmail.com>
2024-06-22 11:28:06 +0530

    add

---
+++ index.js
@@ -0,0 +1,22 @@
+let arr = [1,2,3,4,5,'teja']
+
+let pop = arr.pop(0,1,2,3)
+console.log(pop)
+
+
+arr.push('jagan', 'shekar','hshsh')
+console.log(arr)
+
+
+arr.shift(0,1,2,3)
+
+console.log(arr)
+
+arr.unshift('jagan', 'shekar','teja')
+console.log(arr)
+
+
+arr.splice(1, 4, 'shekarrrrrrrr')
+
+
+console.log(arr)
